2b6474a7ed85f6c7e50d456602a9ebec917b7ec1
angel2388 <rafa2388@gmail.com>
2022-09-22 19:06:11 +0000

    Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function dominions() {
+  let resultado = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++)
+        resultado.push(pronoun[i] + adj[j] + noun[k] + ".com");
+    }
+  }
+  return resultado;
+}
